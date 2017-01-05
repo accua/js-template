25915975263c4f78cacb3ce6b09c8cea93a31198
Andrew Accuardi and Matt Eichsteadt <andrew+matt@>
2017-01-05 09:45:36 -0800

    Add materialize

--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,32 @@ var lib = require('bower-files')({
         "fonts/fontawesome-webfont.woff",
         "fonts/fontawesome-webfont.woff2"
       ]
+    },
+    "materialize" : {
+      "main" : [
+        "bin/materialize.css",
+        "bin/materialize.js",
+        "fonts/roboto/Roboto-Bold.eot",
+        "fonts/roboto/Roboto-Bold.ttf",
+        "fonts/roboto/Roboto-Bold.woff",
+        "fonts/roboto/Roboto-Bold.woff2",
+        "fonts/roboto/Roboto-Light.eot",
+        "fonts/roboto/Roboto-Light.ttf",
+        "fonts/roboto/Roboto-Light.woff",
+        "fonts/roboto/Roboto-Light.woff2",
+        "fonts/roboto/Roboto-Medium.eot",
+        "fonts/roboto/Roboto-Medium.ttf",
+        "fonts/roboto/Roboto-Medium.woff",
+        "fonts/roboto/Roboto-Medium.woff2",
+        "fonts/roboto/Roboto-Regular.eot",
+        "fonts/roboto/Roboto-Regular.ttf",
+        "fonts/roboto/Roboto-Regular.woff",
+        "fonts/roboto/Roboto-Regular.woff2",
+        "fonts/roboto/Roboto-Thin.eot",
+        "fonts/roboto/Roboto-Thin.ttf",
+        "fonts/roboto/Roboto-Thin.woff",
+        "fonts/roboto/Roboto-Thin.woff2"
+      ]
     }
   }
 });
